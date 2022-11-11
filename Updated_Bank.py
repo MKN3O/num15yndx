1c0a1ca74648fffba387e6c76fe66cf73e2edde9
DanKazh <117713084+DanKazh@users.noreply.github.com>
2022-11-11 14:05:50 +0300

    Update Updated_Bank.py

--- Updated_Bank.py
+++ Updated_Bank.py
@@ -1,7 +1,8 @@
-import sys, time
+import sys, time, datetime
 
 import sqlite3
 from PyQt5 import uic
+from datetime import date
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QDialog, QTableWidgetItem
 
@@ -70,14 +71,14 @@ class BogomolBankLogin(QDialog):
         self.loginbutton.clicked.connect(self.login_process)
 
     def login_process(self):
-        name = (self.surnameline.text())
+        self.name = (self.surnameline.text())
         con = sqlite3.connect("bank_db.sqlite")
         cur = con.cursor()
-        if name not in cur.execute('''SELECT username FROM users''').fetchall()[0]:
+        if self.name not in cur.execute('''SELECT username FROM users''').fetchall()[0]:
             cur.execute(f"""INSERT INTO users(username,birthdate,passportns, creditid, depositid)
             VALUES('{self.surnameline.text()}','{self.birthdayline.text()}','{self.passportline.text()}','No', 'No')""")
             con.commit()
-        self.clientpage = BogomolBankClient(name)
+        self.clientpage = BogomolBankClient(self.name)
         self.clientpage.show()
         self.hide()
 
@@ -89,6 +90,8 @@ class BogomolBankClient(QDialog):
         self.name = name
         self.label_2.setText(name)
         self.accountbutton.clicked.connect(self.account)
+        self.creditbutton.clicked.connect(self.credit_making)
+        self.depositbutton.clicked.connect(self.deposit_making)
 
     def account(self):
         con = sqlite3.connect("bank_db.sqlite")
@@ -103,6 +106,39 @@ class BogomolBankClient(QDialog):
                 self.tableWidget.setItem(
                     i, j, QTableWidgetItem(str(elem)))
         self.tableWidget.resizeColumnsToContents()
+
+    def credit_making(self):
+        self.creditpage = BogomolBankCredit()
+        self.creditpage.show()
+        self.hide()
+
+    def deposit_making(self):
+        self.depositpage = BogomolBankDeposit()
+        self.depositpage.show()
+        self.hide()
+
+
+class BogomolBankCredit(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('CreditPage.ui', self)
+        self.pushButton.clicked.connect(self.credit_save)
+
+    def credit_save(self):
+        con = sqlite3.connect("bank_db.sqlite")
+        res = con.cursor().execute(f"""SELECT id FROM users
+                                      WHERE username = '{self.name}'""")
+        cur = con.cursor()
+        self.percent = 10
+        cur.execute(f"""INSERT INTO credits(user_id,startdate,summa,percent,years)
+        VALUES('{res}','{date.today()}','{self.sumline.text()}','{self.percent}', '{self.lengthline.text()}')""")
+        con.commit()
+
+class BogomolBankDeposit(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('DepositPage.ui', self)
+
 
 
 if __name__ == '__main__':
